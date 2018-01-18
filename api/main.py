91afa8e268b2658e570ab87e440e84897726a079
Georges Duverger <georges.duverger@gmail.com>
2018-01-17 20:21:56 -0500

    Use redis kwargs

--- api/main.py
+++ api/main.py
@@ -5,13 +5,12 @@ from rq import Queue
 from api import worker, resources
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.redis import RedisJobStore
-from urllib.parse import urlparse
 
 
 api = falcon.API()
-redis_url_parsed = urlparse(os.getenv('REDISTOGO_URL', 'redis://localhost:6379'))
-scheduler = BackgroundScheduler(jobstores={'redis': RedisJobStore(host=redis_url_parsed.hostname, port=redis_url_parsed.port)})
-queue = Queue(connection=worker.conn)
+redis_kwargs = worker.redis_connection.connection_pool.connection_kwargs
+scheduler = BackgroundScheduler(jobstores={'redis': RedisJobStore(host=redis_kwargs.get('host'), port=redis_kwargs.get('port'))})
+queue = Queue(connection=worker.redis_connection)
 
 api.add_route('/jobs', resources.Jobs())
 api.add_route('/jobs/{job_id}', resources.Job())
--- api/worker.py
+++ api/worker.py
@@ -5,11 +5,11 @@ from rq import Worker, Queue, Connection
 
 
 listen = ['high', 'default', 'low']
-conn = redis.from_url(os.getenv('REDISTOGO_URL', 'redis://localhost:6379'))
+redis_connection = redis.from_url(os.getenv('REDISTOGO_URL', 'redis://localhost:6379'))
 
 
 if __name__ == '__main__':
 
-	with Connection(conn):
+	with Connection(redis_connection):
 		worker = Worker(map(Queue, listen))
 		worker.work()
