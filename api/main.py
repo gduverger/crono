08645d2b748a25e13904226e4e432c29cfecbc5d
Georges Duverger <georges.duverger@gmail.com>
2018-01-20 18:41:32 -0500

    Remove clock.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,13 @@
 import os
 import falcon
+import logging
 
 from rq import Queue
-from api import worker, resources, utils
+from api import resources, utils
 from falcon_auth import FalconAuthMiddleware, TokenAuthBackend
 from postmarker.core import PostmarkClient
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.redis import RedisJobStore
 
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -22,6 +25,11 @@ auth_middleware = FalconAuthMiddleware(token_auth)
 # API
 api = falcon.API(middleware=[auth_middleware])
 
+# Scheduler
+redis_password, redis_host, redis_port = utils.parse_redis_url(os.getenv('REDISTOGO_URL', 'redis://localhost:6379'))
+scheduler = BackgroundScheduler(jobstores={'redis': RedisJobStore(host=redis_host, port=redis_port, password=redis_password)})
+scheduler.start()
+
 # Routes
 api.add_route('/v0/jobs', resources.Jobs())
 api.add_route('/v0/jobs/{job_id}', resources.Job())
@@ -29,3 +37,7 @@ api.add_route('/v0/jobs/{job_id}', resources.Job())
 # Static
 api.add_route('/', resources.Index())
 api.add_static_route('/static', '{}/static'.format(DIR_PATH))
+
+# Logging
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
