5dc7111261f5490bed070a0ea5f95fa943f88116
Georges Duverger <georges@nautiluslabs.co>
2018-05-07 15:57:07 -0400

    add os.getenv, remove parse_redis_url

--- app.py
+++ app.py
@@ -58,10 +58,6 @@ def get_jobs():
 
 
 def post_job(job: Job):
-	# print(job['trigger']['seconds'])
-	# print(job['task']['type'])
-	# print(job['task']['parameters'])
-
 	name = job['name'] if hasattr(job, 'name') else str(datetime.datetime.now())
 	interval = celery.schedules.schedule(run_every=job['trigger']['seconds']) # seconds
 	entry = redbeat.schedulers.RedBeatSchedulerEntry(name=name, task=job['task']['type'], schedule=interval, kwargs=job['task']['parameters'], app=scheduler.queue)
@@ -83,14 +79,14 @@ def delete_job(job_id: str):
 
 routes = [
 	Route('/', 'GET', welcome),
-	Route('/beta/jobs', 'GET', get_jobs),
-	Route('/beta/jobs/', 'GET', get_jobs, name='get_jobs_slash'),
-	Route('/beta/jobs', 'POST', post_job),
-	Route('/beta/jobs/', 'POST', post_job, name='post_job_slash'),
-	Route('/beta/jobs/{job_id}', 'GET', get_job),
-	Route('/beta/jobs/{job_id}/', 'GET', get_job, name='get_job_slash'),
-	Route('/beta/jobs/{job_id}', 'DELETE', delete_job),
-	Route('/beta/jobs/{job_id}/', 'DELETE', delete_job, name='delete_job_slash'),
+	Route('/jobs', 'GET', get_jobs),
+	Route('/jobs/', 'GET', get_jobs, name='get_jobs_slash'),
+	Route('/jobs', 'POST', post_job),
+	Route('/jobs/', 'POST', post_job, name='post_job_slash'),
+	Route('/jobs/{job_id}', 'GET', get_job),
+	Route('/jobs/{job_id}/', 'GET', get_job, name='get_job_slash'),
+	Route('/jobs/{job_id}', 'DELETE', delete_job),
+	Route('/jobs/{job_id}/', 'DELETE', delete_job, name='delete_job_slash'),
 	Include('/docs', docs_urls),
 	Include('/static', static_urls)
 ]
@@ -99,7 +95,7 @@ routes = [
 app = App(routes=routes)
 
 
-postmark = PostmarkClient(server_token='7be71b51-6cb1-4a7c-b1ec-21e4c1b4ec5d')
+postmark = PostmarkClient(server_token=os.getenv('POSTMARK_SERVER_TOKEN'))
 
 
 if __name__ == '__main__':
--- scheduler.py
+++ scheduler.py
@@ -4,13 +4,13 @@ import redbeat
 
 
 queue = celery.Celery('crono-api', # NOTE Name of the main module if running as __main__
-		broker=os.getenv('REDIS_URL', 'redis://localhost:6379/0'),
-		backend=os.getenv('REDIS_URL', 'redis://localhost:6379/0'),
+		broker=os.getenv('CELERY_REDIS_URL'),
+		backend=os.getenv('CELERY_REDIS_URL'),
 		include=['tasks'])
 
 queue.conf.broker_pool_limit = 1
 queue.conf.redis_max_connections = 1
-queue.conf.redbeat_redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379/1')
+queue.conf.redbeat_redis_url = os.getenv('REDBEAT_REDIS_URL')
 # queue.conf.beat_max_loop_interval = 5
 # queue.conf.redbeat_lock_timeout = 5
 # queue.conf.result_backend = 'redis://localhost:6379/0'
