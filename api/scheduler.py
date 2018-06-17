8f3a7a708c434400406a60a5cd910a1463c1537a
Georges Duverger <georges.duverger@gmail.com>
2018-06-17 10:34:09 -0400

    Remove redbeat_key_prefix

--- api/scheduler.py
+++ api/scheduler.py
@@ -8,10 +8,9 @@ queue = celery.Celery('api', # NOTE Name of the main module if running as __main
 		backend=os.getenv('REDIS_URL'),
 		include=['api.tasks'])
 
-queue.conf.broker_pool_limit = 1
-queue.conf.redis_max_connections = 1
 queue.conf.redbeat_redis_url = os.getenv('REDIS_URL')
-queue.conf.redbeat_key_prefix = 'crono:'
+# queue.conf.broker_pool_limit = 1
+# queue.conf.redis_max_connections = 1
 # queue.conf.beat_max_loop_interval = 5
 # queue.conf.redbeat_lock_timeout = 5
 # queue.conf.result_backend = 'redis://localhost:6379/0'
