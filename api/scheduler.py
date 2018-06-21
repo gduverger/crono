54f1678450d285647cb98ddcf013a0d1dcd8b9da
Georges Duverger <georges.duverger@gmail.com>
2018-06-21 08:13:42 -0400

    redis_max_connections

--- api/scheduler.py
+++ api/scheduler.py
@@ -9,8 +9,8 @@ queue = celery.Celery('api', # NOTE Name of the main module if running as __main
 		include=['api.tasks'])
 
 queue.conf.redbeat_redis_url = os.getenv('REDIS_URL')
+queue.conf.redis_max_connections = 20
 # queue.conf.broker_pool_limit = 1
-# queue.conf.redis_max_connections = 1
 # queue.conf.beat_max_loop_interval = 5
 # queue.conf.redbeat_lock_timeout = 5
 # queue.conf.result_backend = 'redis://localhost:6379/0'
