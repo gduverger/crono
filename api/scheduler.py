7270998d2f602bc70b67712fce0d3ca68c5c8e63
Georges Duverger <georges.duverger@gmail.com>
2018-07-06 18:57:39 +0200

    Add note

--- api/scheduler.py
+++ api/scheduler.py
@@ -9,7 +9,11 @@ queue = celery.Celery('api', # NOTE Name of the main module if running as __main
 		include=['api.tasks'])
 
 queue.conf.redbeat_redis_url = os.getenv('REDIS_URL')
+
+# NOTE Heroku Redis' Hobby Dev has a limit of 20 connections
+# https://elements.heroku.com/addons/heroku-redis
 queue.conf.redis_max_connections = 18
+
 # queue.conf.broker_pool_limit = 1
 # queue.conf.beat_max_loop_interval = 5
 # queue.conf.redbeat_lock_timeout = 5
