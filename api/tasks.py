ab14711ef0bffe5ba80e9b44def4d513a5ddf55e
Georges Duverger <georges.duverger@gmail.com>
2018-07-11 07:51:59 -0400

    Update logs

--- api/tasks.py
+++ api/tasks.py
@@ -18,32 +18,37 @@ logger.setLevel(logging.INFO)
 
 @api.scheduler.queue.task
 def log(job_key, message=None):
+	extra = {'job_key': job_key, 'message': message}
 
 	try:
 		models.Job.get_by_key(job_key).add_log()
-		logger.info('log')
+		logger.info('Task: log', extra=extra)
 
 	except Exception as error:
-		logger.critical(error, extra={'job_key': job_key, 'url': url})
+		logger.error(error, extra=extra, exc_info=True)
 
 
 @api.scheduler.queue.task
 def get(job_key, url=None):
+	extra = {'job_key': job_key, 'url': url}
 
 	try:
 		models.Job.get_by_key(job_key).add_log()
 		requests.get(url, timeout=60) # 1 minute
+		logger.info('Task: get', extra=extra)
 
 	except Exception as error:
-		logger.critical(error, extra={'job_key': job_key, 'url': url})
+		logger.error(error, extra=extra, exc_info=True)
 
 
 @api.scheduler.queue.task
 def email(job_key, to=None, subject=None, body=None):
-	
+	extra = {'job_key': job_key, 'to': to, 'subject': subject, 'body': body}
+
 	try:
 		models.Job.get_by_key(job_key).add_log()
 		postmark.emails.send(From=os.getenv('FROM_EMAIL_ADDRESS'), To=to, Subject=subject, TextBody=body)
+		logger.info('Task: email', extra=extra)
 
 	except Exception as error:
-		logger.critical(error, extra={'job_key': job_key, 'to': to, 'subject': subject, 'body': body})
+		logger.error(error, extra=extra, exc_info=True)
