ffdb8d0083e2bbbe279bcb0c3b672c101d5616d1
Georges Duverger <georges.duverger@gmail.com>
2018-11-21 15:41:48 -0500

    Up max_connections

--- api/scheduler.py
+++ api/scheduler.py
@@ -3,16 +3,18 @@ import celery
 import redbeat
 
 
-queue = celery.Celery('api', # NOTE Name of the main module if running as __main__
+queue = celery.Celery('api', # DOC Name of the main module if running as __main__. This is used as the prefix for auto-generated task names.
 		broker=os.getenv('REDIS_BROKER_URL'),
 		backend=os.getenv('REDIS_BACKEND_URL'),
 		include=['api.tasks'])
 
 # queue.conf.redbeat_redis_url = os.getenv('REDIS_URL')
+# queue.conf.beat_max_loop_interval = 5
+# queue.conf.redbeat_lock_timeout = 5
 
 # NOTE Heroku Redis' Hobby Dev has a limit of 20 connections
 # https://elements.heroku.com/addons/heroku-redis
-queue.conf.redis_max_connections = int(os.getenv('REDIS_MAX_CONNECTIONS', 18))
+queue.conf.redis_max_connections = int(os.getenv('REDIS_MAX_CONNECTIONS', 20))
 
 # NOTE Celery seems to have a memory leak with redis broker
 # http://docs.celeryproject.org/en/3.1/configuration.html#celeryd-max-tasks-per-child
@@ -20,8 +22,6 @@ queue.conf.redis_max_connections = int(os.getenv('REDIS_MAX_CONNECTIONS', 18))
 queue.conf.worker_max_tasks_per_child = 100
 
 # queue.conf.broker_pool_limit = 1
-# queue.conf.beat_max_loop_interval = 5
-# queue.conf.redbeat_lock_timeout = 5
 # queue.conf.result_backend = 'redis://localhost:6379/0'
 # queue.conf.beat_scheduler = 'redbeat.schedulers.RedBeatScheduler'
 
