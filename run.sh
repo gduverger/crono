1b033720d8d0c89390c28e41c79c6469251b3642
Georges Duverger <georges.duverger@gmail.com>
2018-12-30 13:41:32 -0500

    Requirements

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 redis-server &
 celery worker --app=api.scheduler:queue --loglevel=DEBUG --detach
 celery beat --app=api.scheduler:queue --scheduler=redbeat.schedulers.RedBeatScheduler --loglevel=DEBUG --detach
-python api/app.py
+gunicorn api.app:app --bind=127.0.0.1:5000
