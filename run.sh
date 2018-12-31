8fcbf30028db4c6c49d9f55bc95589c9e1ce7a3e
Georges Duverger <georges.duverger@gmail.com>
2018-12-31 14:33:47 -0500

    Styles

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 redis-server &
 celery worker --app=api.scheduler:queue --loglevel=DEBUG --detach
 celery beat --app=api.scheduler:queue --scheduler=redbeat.schedulers.RedBeatScheduler --loglevel=DEBUG --detach
-gunicorn api.app:app --bind=127.0.0.1:5000
+python api/app.py
