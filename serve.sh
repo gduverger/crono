f3ec1ace508f2ab284b4ef209073e8cd4ba1e86a
Georges Duverger <georges.duverger@gmail.com>
2019-01-06 09:36:29 -0500

    uvicorn

--- serve.sh
+++ serve.sh
@@ -2,4 +2,5 @@
 redis-server &
 celery worker --app=api.scheduler:queue --loglevel=DEBUG --detach
 celery beat --app=api.scheduler:queue --scheduler=redbeat.schedulers.RedBeatScheduler --loglevel=DEBUG --detach
+# uvicorn api.app:app --debug
 python api/app.py
