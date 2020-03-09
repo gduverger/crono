8004298d759505071719714fbc51931ad30350b5
Georges Duverger <georges.duverger@gmail.com>
2020-03-09 13:30:16 -0400

    library

--- Procfile
+++
@@ -1,3 +0,0 @@
-worker: celery worker --app=api.scheduler:queue
-clock: celery beat --app=api.scheduler:queue --scheduler=redbeat.schedulers.RedBeatScheduler
-web: gunicorn api.app:app
--- serve.sh
+++ serve.sh
@@ -1,6 +1,3 @@
-# . run.sh
 redis-server &
-celery worker --app=api.scheduler:queue --loglevel=DEBUG --detach
-celery beat --app=api.scheduler:queue --scheduler=redbeat.schedulers.RedBeatScheduler --loglevel=DEBUG --detach
-# uvicorn api.app:app --debug
-python api/app.py
+celery worker --app=crono.scheduler:queue --loglevel=DEBUG --detach
+celery beat --app=crono.scheduler:queue --scheduler=redbeat.schedulers.RedBeatScheduler --loglevel=DEBUG --detach
