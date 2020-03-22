8e3909f8ec24d591600e2e38bde94b268faaf442
Georges Duverger <georges.duverger@gmail.com>
2020-03-22 09:23:25 -0400

    remove attrs

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 ## Environmental variables
 	
-	# REDIS_URL
+	REDIS_URL
 	REDIS_BROKER_URL
 	REDIS_BACKEND_URL
 	REDIS_MAX_CONNECTIONS
+	REDIS_MAX_CONNECTIONS (default: 20)
+	BEAT_MAX_LOOP_INTERVAL (default: 300)
+	BROKER_POOL_LIMIT (default: None)
+	TASK_IGNORE_RESULT (default: True)
+	WORKER_MAX_TASKS_PER_CHILD (default: 100)
 
 ## How to start
 
--- crono/queue.py
+++ crono/queue.py
@@ -2,8 +2,9 @@ import os
 import celery
 import redbeat
 
-
-# DOC Name of the main module if running as __main__. This is used as the prefix for auto-generated task names.
+# NOTE
+# Name of the main module if running as __main__.
+# This is used as the prefix for auto-generated task names.
 queue = celery.Celery('crono',
 	broker=os.getenv('REDIS_BROKER_URL'),
 	backend=os.getenv('REDIS_BACKEND_URL'),
@@ -24,12 +25,12 @@ queue = celery.Celery('crono',
 
 # NOTE Heroku Redis' Hobby Dev has a limit of 20 connections
 # https://elements.heroku.com/addons/heroku-redis
+queue.conf.redis_max_connections = int(os.getenv('REDIS_MAX_CONNECTIONS', 20))
 
 # NOTE Celery seems to have a memory leak with redis broker
 # http://docs.celeryproject.org/en/3.1/configuration.html#celeryd-max-tasks-per-child
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#worker-max-memory-per-child
 queue.conf.worker_max_tasks_per_child = int(os.getenv('WORKER_MAX_TASKS_PER_CHILD', 100))
-queue.conf.redis_max_connections = int(os.getenv('REDIS_MAX_CONNECTIONS', 20))
 queue.conf.broker_pool_limit = os.getenv('BROKER_POOL_LIMIT', None)
 queue.conf.task_ignore_result = os.getenv('TASK_IGNORE_RESULT', True)
 queue.conf.beat_max_loop_interval = int(os.getenv('BEAT_MAX_LOOP_INTERVAL', 300)) # in seconds (5 minutes by default)
