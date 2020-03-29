277f2697fbbcdf9cb4f060a790f47353561e170b
Georges Duverger <georges.duverger@gmail.com>
2020-03-29 11:10:00 -0400

    0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
 	name='crono',
-	version='0.1.0',
+	version='0.1.1',
 	author='Georges Duverger',
 	author_email='georges.duverger@gmail.com',
 	description='Programmatic time-based job scheduler',
